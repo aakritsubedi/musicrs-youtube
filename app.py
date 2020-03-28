3b575714c019658de89bed483ac2556ddd183fe4
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-03-28 18:44:50 +0545

    Update Server App

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from apiclient.discovery import build
 from apiclient.errors import HttpError
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# DEVELOPER_KEY =  os.getenv("DEVELOPER_KEY")
-DEVELOPER_KEY = "AIzaSyClgo5jx9Ojs_T2qQ4kTrRBCvWEJmThDj8"
+DEVELOPER_KEY = os.getenv("DEVELOPER_KEY")
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
@@ -16,30 +15,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello Home Page'
+    return render_template('index.html')
 
 
 @app.route('/youtube/id/<string:id>')
 def youtubeData(id):
-    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
-    request = youtube.videos().list(part="snippet", id="Z1RJmh_OqeA")
+    youtube = build(YOUTUBE_API_SERVICE_NAME,
+                    YOUTUBE_API_VERSION,
+                    developerKey=DEVELOPER_KEY)
+    request = youtube.videos().list(part="snippet", id= id)
     response = request.execute()
-
     return response
+
 
 @app.route('/youtube/title/<string:title>')
 def youtubeSearchList(title):
-    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
+    youtube = build(YOUTUBE_API_SERVICE_NAME,
+                    YOUTUBE_API_VERSION,
+                    developerKey=DEVELOPER_KEY)
 
-    request = youtube.search().list(
-    q=title,
-    type="video",
-    pageToken=None,
-    order = "relevance",
-    part="id,snippet",
-    maxResults=5)
+    request = youtube.search().list(q=title,
+                                    type="video",
+                                    pageToken=None,
+                                    order="relevance",
+                                    part="id,snippet",
+                                    maxResults=5)
 
     response = request.execute()
 
     return response
-
