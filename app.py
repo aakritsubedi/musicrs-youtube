8a6f5e016eed4c70092c1f6d5b788224315c2002
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-03-28 13:02:26 +0545

    Main: Update routes for search by Id and Title

--- app.py
+++ app.py
@@ -1,11 +1,46 @@
+import os
 from flask import Flask
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEVELOPER_KEY =  os.getenv("DEVELOPER_KEY")
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
 
 app = Flask(__name__)
 
-@app.route('/')
-def index(): 
-  return 'Hello Aakrit Subedi'
 
-if __name__ == '__main__': 
-  app.run(debug=True)
-  
+@app.route('/')
+def index():
+    return 'Hello Home Page'
+
+
+@app.route('/youtube/id/<string:id>')
+def youtubeData(id):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
+    request = youtube.videos().list(part="snippet", id="Z1RJmh_OqeA")
+    response = request.execute()
+
+    return response
+
+@app.route('/youtube/title/<string:title>')
+def youtubeSearchList(title):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
+
+    request = youtube.search().list(
+    q=title,
+    type="video",
+    pageToken=None,
+    order = "relevance",
+    part="id,snippet",
+    maxResults=5)
+
+    response = request.execute()
+
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
